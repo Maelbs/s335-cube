98efd41eec25c96757073b73713518fe20bdab78
Maël <maelbouviersobrino@hotmail.com>
2025-11-26 16:27:53 +0000

    listArticle view Article + necessary classes

---
+++ app/Models/Fourche.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fourche extends Model
+{
+    use HasFactory;
+
+    protected $table = 'fourche';
+    public $timestamps = false;
+    protected $primaryKey = 'id_fourche';
+
+    protected $fillable = [
+        'nom_fourche',
+    ];
+
+    protected $casts = [
+        'id_fourche' => 'integer',
+        'nom_fourche' => 'string',
+    ];
+
+    public function varianteVelos()
+    {
+        // La clé étrangère est 'id_fourche' dans la table 'Variante_velo'
+        return $this->hasMany(VarianteVelo::class, 'id_fourche');
+    }
+
+    public function __toString()
+    {
+        return sprintf(
+            "Fourche [ID: %s] : Nom: %s",
+            $this->id_fourche,
+            $this->nom_fourche
+        );
+    }
+}
---
+++ app/Models/VarianteVelo.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class VarianteVelo extends Article
+{
+    use HasFactory;
+
+    protected $table = 'Variante_velo';
+    protected $primaryKey = 'reference';
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'reference',
+        'id_batterie',
+        'id_modele',
+        'id_fourche',
+        'id_couleur',
+        'nom_article',
+        'prix',
+        'qte_stock',
+        'dispo_en_ligne',
+        'poids',
+        'dispo_magasin',
+    ];
+
+    protected $casts = [
+        'prix' => 'float',
+        'qte_stock' => 'integer',
+        'dispo_en_ligne' => 'boolean',
+        'poids' => 'float',
+        'dispo_magasin' => 'boolean',
+    ];
+
+
+    public function batterie()
+    {
+        return $this->belongsTo(Batterie::class, 'id_batterie');
+    }
+
+    public function modele()
+    {
+        return $this->belongsTo(Modele::class, 'id_modele');
+    }
+
+    public function fourche()
+    {
+        return $this->belongsTo(Fourche::class, 'id_fourche');
+    }
+
+    public function couleur()
+    {
+        return $this->belongsTo(Couleur::class, 'id_couleur');
+    }
+
+    
+    public function parent()
+    {
+        return $this->belongsTo(Article::class, 'reference', 'reference');
+    }
+
+    public function __toString()
+    {
+        return sprintf(
+            "Variante Vélo [Ref: %s] : %s | Modèle ID: %s | Couleur ID: %s | Batterie ID: %s | Fourche ID: %s | Prix: %s € | Stock: %s | Poids: %s kg | Dispo Web: %s",
+            $this->reference,
+            $this->nom_article,
+            $this->id_batterie,
+            $this->id_modele,
+            $this->id_fourche,
+            $this->id_couleur,
+            number_format($this->prix, 2, ',', ' '),
+            $this->qte_stock,
+            $this->poids,
+            $this->dispo_en_ligne ? 'Oui' : 'Non'
+        );
+    }
+}
