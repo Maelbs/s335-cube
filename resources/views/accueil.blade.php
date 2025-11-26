9e1e84e60ca493bf729d19a7feaa6491be4996fb
IanBoultchynski <ian.boultchynski@gmail.com>
2025-11-26 08:41:52 +0000

    update backgournd header

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -16,25 +16,51 @@
                     <img src="{{ asset('images/cubelogo.png') }}" alt="CUBE Logo">
                 </div>
                 <li><a href="{{ url('/Velo') }}">Vélos</a></li>
-                 <li><a href="{{ url('/bikes') }}">Vélos electriques</a></li>
-           
+                <li><a href="{{ url('/bikes') }}">Vélos electriques</a></li>
                 <li><a href="{{ url('/contact') }}">Accessoires</a></li>
-            
             </ul>
         </div>
 
         <div class="menu-user">
             
                 <a id="magasin" href="{{ url('/login') }}">Choisir un magasin</a></li>
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="#000000"><g fill="none" stroke="#000000" stroke-width="1.5"><path d="M20.485 3h-3.992l.5 5s1 1 2.5 1a3.23 3.23 0 0 0 2.139-.806a.503.503 0 0 0 .15-.465L21.076 3.5a.6.6 0 0 0-.591-.5Z"/><path d="m16.493 3l.5 5s-1 1-2.5 1s-2.5-1-2.5-1V3h4.5Z"/><path d="M11.993 3v5s-1 1-2.5 1s-2.5-1-2.5-1l.5-5h4.5Z"/><path d="M7.493 3H3.502a.6.6 0 0 0-.592.501L2.205 7.73a.504.504 0 0 0 .15.465c.328.29 1.061.806 2.138.806c1.5 0 2.5-1 2.5-1l.5-5Z"/><path d="M3 9v10a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V9"/><path stroke-miterlimit="16" d="M14.833 21v-6a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v6"/></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
+                    <g>
+                        <path d="M20.485 3h-3.992l.5 5s1 1 2.5 1a3.23 3.23 0 0 0 2.139-.806a.503.503 0 0 0 .15-.465L21.076 3.5a.6.6 0 0 0-.591-.5Z"/>
+                        <path d="m16.493 3l.5 5s-1 1-2.5 1s-2.5-1-2.5-1V3h4.5Z"/>
+                        <path d="M11.993 3v5s-1 1-2.5 1s-2.5-1-2.5-1l.5-5h4.5Z"/>
+                        <path d="M7.493 3H3.502a.6.6 0 0 0-.592.501L2.205 7.73a.504.504 0 0 0 .15.465c.328.29 1.061.806 2.138.806c1.5 0 2.5-1 2.5-1l.5-5Z"/>
+                        <path d="M3 9v10a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V9"/>
+                        <path d="M14.833 21v-6a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v6"/>
+                    </g>
+                </svg>
                 
                 <div class="icone">
-                    {{-- icone loupe  POUR CHERCHER --}}
-                 <a href="{{ url('/recherche') }}"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 20 20" fill="#0f0f0f"><g fill="#0f0f0f"><path fill-rule="evenodd" d="M4.828 4.828A5 5 0 1 0 11.9 11.9a5 5 0 0 0-7.07-7.07Zm6.364 6.364a4 4 0 1 1-5.656-5.657a4 4 0 0 1 5.656 5.657Z" clip-rule="evenodd"/><path d="M11.192 12.627a1 1 0 0 1 1.415-1.414l2.828 2.829a1 1 0 1 1-1.414 1.414l-2.829-2.829Z"/></g></svg></a>
-                 {{-- icone utilisateur --}}
-                 <a href="{{ url('/login') }}"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="#000000"><g fill="none" stroke="#000000" stroke-width="2"><path stroke-linejoin="round" d="M4 18a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z"/><circle cx="12" cy="7" r="3"/></g></svg></a>
-                 {{-- icone panier --}}
-                 <a href="{{ url('/panier') }}"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 48 48" fill="#000000"><g fill="none"><path d="M39 32H13L8 12h36l-5 20Z"/><path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M3 6h3.5L8 12m0 0l5 20h26l5-20H8Z"/><circle cx="13" cy="39" r="3" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/><circle cx="39" cy="39" r="3" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/></g></svg></a>
+                    
+
+                   
+                    <a href="{{ url('/recherche') }}"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 20 20">
+                        <g>
+                            <path fill-rule="evenodd" d="M4.828 4.828A5 5 0 1 0 11.9 11.9a5 5 0 0 0-7.07-7.07Zm6.364 6.364a4 4 0 1 1-5.656-5.657a4 4 0 0 1 5.656 5.657Z" clip-rule="evenodd"/>
+                            <path d="M11.192 12.627a1 1 0 0 1 1.415-1.414l2.828 2.829a1 1 0 1 1-1.414 1.414l-2.829-2.829Z"/>
+                        </g>
+                    </svg></a>
+                 
+                    <a href="{{ url('/login') }}"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
+                        <g stroke-width="2">
+                            <path stroke-linejoin="round" d="M4 18a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z"/>
+                            <circle cx="12" cy="7" r="3"/>
+                        </g>
+                    </svg></a>
+             
+                    <a href="{{ url('/panier') }}"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 48 48">
+                        <g>
+                            <path d="M39 32H13L8 12h36l-5 20Z"/>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M3 6h3.5L8 12m0 0l5 20h26l5-20H8Z"/>
+                            <circle cx="13" cy="39" r="3" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/>
+                            <circle cx="39" cy="39" r="3" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"/>
+                        </g>
+                    </svg></a>
                 </div>
         </div>
         </nav>
@@ -43,7 +69,8 @@
     </header>
 
 
-
+<main>
+    
 
 
     <footer>
