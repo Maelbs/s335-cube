aa69158b977646dc9c4b2c497626eee2f8652e77
VMLeo7412 <leomorard74@icloud.com>
2025-11-27 08:15:51 +0000

    Primary key null dans les fichiers de tables de liaison

--- app/Models/TypeLivraisonMagasin.php
+++ app/Models/TypeLivraisonMagasin.php
@@ -10,8 +10,15 @@ class TypeLivraisonMagasin extends Pivot
     public $timestamps = false;
     public $incrementing = false;
 
+    protected $primaryKey = null;
+
     protected $fillable = [
         'id_type_livraison', // Clé étrangère
         'id_magasin', // Clé étrangère
     ];
+
+    public function __toString(): string
+    {
+        return "{$this->id_type_livraison}, {$this->id_magasin}";
+    }
 }
