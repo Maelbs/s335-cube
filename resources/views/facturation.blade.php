cb8a90bc6f79cc2790a9860769b37eb6b74b2d08
elvis <Elvis-Aaron.Andrianantoandro@etu.univ-smb.fr>
2025-12-07 23:37:11 +0000

    Amélioration visuel formulaire adresse

--- resources/views/facturation.blade.php
+++ resources/views/facturation.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +15,8 @@
 
     <section id="wrapper" class="container" style="margin-top: 10rem">
         <div class="row justify-content-center">
-            <div id="content-wrapper" class="col-12 col-lg-8"> <section class="register-form text-center">
+            <div id="content-wrapper" class="col-12 col-lg-8">
+                <section class="register-form text-center">
 
                     @if ($errors->any())
                         <div class="alert alert-danger" style="text-align: left;">
@@ -26,43 +28,48 @@
                         </div>
                     @endif
 
-                    <form class="js-customer-form needs-validation card card-account text-left"
-                        id="customer-form" action="{{ route('facturation.send') }}" method="POST" autocomplete="off">
+                    <form class="js-customer-form needs-validation card card-account text-left" id="customer-form"
+                        action="{{ route('facturation.send') }}" method="POST" autocomplete="off">
                         @csrf
 
                         <div id="address-section">
-                            
-                            <h2 class="h4 font-weight-bold text-uppercase mb-4" style="border-bottom: 2px solid #333; padding-bottom: 10px; text-align: left;">
+
+                            <h2 class="h4 font-weight-bold text-uppercase mb-4"
+                                style="border-bottom: 2px solid #333; padding-bottom: 10px; text-align: left;">
                                 Livraison
                             </h2>
 
                             <section class="form-fields">
-                                <div class="form-group ">
-                                    <label class="required font-weight-bold " for="rue">Rue </label>
-                                    <input class="form-control" name="rue" type="text" value="{{ old('rue') }}" id="rue" required placeholder="Ex: 10 rue de la paix...">
-                                </div>
-
-                                <div class="form-row">
-                                    <div class="col-md-6 form-group">
-                                        <label class="required font-weight-bold" for="zipcode">Code postal </label>
-                                        <input class="form-control" name="zipcode" type="text" value="{{ old('zipcode') }}" id="zipcode" required  style="background-color: #e9ecef;">
-                                    </div>
-                                    <div class="col-md-6 form-group">
-                                        <label class="required font-weight-bold" for="city">Ville </label>
-                                        <input class="form-control" name="city" type="text" value="{{ old('city') }}" id="city" required  style="background-color: #e9ecef;">
-                                    </div>
+                                <div class="form-group">
+                                    <label class="required font-weight-bold" for="rue">Rue</label>
+                                    <input class="form-control" name="rue" type="text" value="{{ old('rue') }}" id="rue"
+                                        required placeholder="Ex: 10 rue de la paix...">
                                 </div>
 
                                 <div class="form-group">
-                                    <label class="required font-weight-bold" for="country">Pays </label>
-                                    <input class="form-control" name="country" type="text" value="{{ old('country') }}" id="country" required  style="background-color: #e9ecef;">
+                                    <label class="required font-weight-bold" for="zipcode">Code postal</label>
+                                    <input class="form-control" name="zipcode" type="text" value="{{ old('zipcode') }}"
+                                        id="zipcode" required ly style="background-color: #e9ecef;">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="required font-weight-bold" for="country">Pays</label>
+                                    <input class="form-control" name="country" type="text" value="{{ old('country') }}"
+                                        id="country" required ly style="background-color: #e9ecef;">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="required font-weight-bold" for="city">Ville</label>
+                                    <input class="form-control" name="city" type="text" value="{{ old('city') }}"
+                                        id="city" required ly style="background-color: #e9ecef;">
                                 </div>
                             </section>
 
-                            <div class="form-group mt-3">
-                                <label class="custom-toggle-container" for="use_same_address">
+                            <div class="form-group mt-3 custom-toggle-container">
+                                <label class="custom-toggle-container" for="use_same_address" style="width: 100%;">
                                     <div class="toggle-switch">
-                                        <input type="checkbox" name="use_same_address" id="use_same_address" value="1" {{ old('use_same_address', '1') == '1' ? 'checked' : '' }}>
+                                        <input type="checkbox" name="use_same_address" id="use_same_address" value="1"
+                                            {{ old('use_same_address', '1') == '1' ? 'checked' : '' }}>
                                         <span class="slider"></span>
                                     </div>
                                     <span class="toggle-label">Utiliser aussi comme adresse de facturation</span>
@@ -70,37 +77,47 @@
                             </div>
 
                             <div id="billing-address-wrapper">
-                                <h2 class="h4 font-weight-bold text-uppercase mb-4 mt-4" style="border-bottom: 2px solid #333; padding-bottom: 10px; text-align: left;">
+                                <h2 class="h4 font-weight-bold text-uppercase mb-4 mt-4"
+                                    style="border-bottom: 2px solid #333; padding-bottom: 10px; text-align: left;">
                                     Facturation
                                 </h2>
                                 <section class="form-fields">
                                     <div class="form-group">
-                                        <label class="required font-weight-bold" for="billing_rue">Rue </label>
-                                        <input class="form-control" name="billing_rue" type="text" value="{{ old('billing_rue') }}" id="billing_rue" placeholder="Rechercher une adresse...">
+                                        <label class="required font-weight-bold" for="billing_rue">Rue</label>
+                                        <input class="form-control" name="billing_rue" type="text"
+                                            value="{{ old('billing_rue') }}" id="billing_rue"
+                                            placeholder="Ex: 10 rue de la paix...">
                                     </div>
-                                    <div class="form-row">
-                                        <div class="col-md-6 form-group">
-                                            <label class="required font-weight-bold" for="billing_zipcode">Code postal </label>
-                                            <input class="form-control" name="billing_zipcode" type="text" value="{{ old('billing_zipcode') }}" id="billing_zipcode"  style="background-color: #e9ecef;">
-                                        </div>
-                                        <div class="col-md-6 form-group">
-                                            <label class="required font-weight-bold" for="billing_city">Ville </label>
-                                            <input class="form-control" name="billing_city" type="text" value="{{ old('billing_city') }}" id="billing_city"  style="background-color: #e9ecef;">
-                                        </div>
-                                    </div>
+
                                     <div class="form-group">
-                                        <label class="required font-weight-bold" for="billing_country">Pays </label>
-                                        <input class="form-control" name="billing_country" type="text" value="{{ old('billing_country') }}" id="billing_country"  style="background-color: #e9ecef;">
+                                        <label class="required font-weight-bold" for="billing_zipcode">Code postal</label>
+                                        <input class="form-control" name="billing_zipcode" type="text"
+                                            value="{{ old('billing_zipcode') }}" id="billing_zipcode"
+                                            style="background-color: #e9ecef;">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="required font-weight-bold" for="billing_country">Pays</label>
+                                        <input class="form-control" name="billing_country" type="text"
+                                            value="{{ old('billing_country') }}" id="billing_country"
+                                            style="background-color: #e9ecef;">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label class="required font-weight-bold" for="billing_city">Ville</label>
+                                        <input class="form-control" name="billing_city" type="text"
+                                            value="{{ old('billing_city') }}" id="billing_city"
+                                            style="background-color: #e9ecef;">
                                     </div>
                                 </section>
                             </div>
 
-                            <footer class="form-footer mt-5">
-                                <button class="btn btn-primary btn-valider w-100 py-3 text-uppercase" type="submit">
-                                    Continuer
-                                </button>
-                            </footer>
                         </div>
+                        <footer class="form-footer mt-5">
+                            <button class="btn btn-primary btn-valider w-100 py-3 text-uppercase" type="submit">
+                                Continuer
+                            </button>
+                        </footer>
                     </form>
                 </section>
             </div>
@@ -109,6 +126,7 @@
 
     <script src="{{ asset('js/header.js') }}" defer></script>
     <script src="{{ asset('js/facturation.js') }}" defer></script>
-    
+
 </body>
+
 </html>
